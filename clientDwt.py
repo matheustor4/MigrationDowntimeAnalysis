700c6d0d071f884468366d9ad9a0cfbf398eee67
Matheus D'Eça Torquato de Melo <matheustor4.professor@gmail.com>
2025-04-11 11:58:02 -0300

    Update and rename clientUDP.py to clientDwt.py

--- clientDwt.py
+++ clientDwt.py
@@ -2,10 +2,12 @@ import socket
 import time
 import datetime
 
+# Adjust UDP_IP to the server IP
 UDP_IP = "192.168.0.101"
 UDP_PORT = 5005
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# Adjust timeout 
 sock.settimeout(0.1)
 
 
@@ -13,18 +15,18 @@ while True:
     try:
      sock.sendto(b"hora_ms", (UDP_IP, UDP_PORT))
      data, addr = sock.recvfrom(1024)
-     hora_servidor = data.decode()  # Converte a string recebida para inteiro
-     print(f"Hora do servidor (ms): {hora_servidor}")
+     hora_servidor = data.decode() 
+     print(f"Server time (ms): {hora_servidor}")
      # Salva a hora no arquivo de texto
-     with open("hora_servidor.txt", "a") as arquivo:
+     with open("server_time.txt", "a") as arquivo:
          arquivo.write(str(hora_servidor) + "\n")
      #print(f"escrita ok")
     except socket.timeout:
-     print("Timeout: Servidor não respondeu")
-     with open("hora_servidor.txt", "a") as arquivo:
+     print("Timeout: Server did not respond")
+     with open("server_time.txt", "a") as arquivo:
          arquivo.write("timeout\n")
     except socket.error as e:
      print(f"Erro de socket: {e}")
-     with open("hora_servidor.txt", "a") as arquivo:
+     with open("server_time.txt", "a") as arquivo:
          arquivo.write(f"erro: {e}\n")
     time.sleep(0.1)
