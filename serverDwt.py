f3bf2712a75eab4b1a34fc64fd743077fb5c32be
Matheus D'Eça Torquato de Melo <matheustor4.professor@gmail.com>
2025-04-11 12:01:28 -0300

    Update serverDwt.py

--- serverDwt.py
+++ serverDwt.py
@@ -2,17 +2,17 @@ import socket
 import time
 import datetime
 
-UDP_IP = "127.0.0.1"
+UDP_IP = "0.0.0.0"
 UDP_PORT = 5005
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP, UDP_PORT))
 
-print(f"Servidor UDP ouvindo em {UDP_IP}:{UDP_PORT}")
+print(f"Server listen {UDP_IP}:{UDP_PORT}")
 
 while True:
     data, addr = sock.recvfrom(1024)
-    print(f"Mensagem recebida de {addr}")
+    print(f"Message from {addr}")
     now = datetime.datetime.now()
     time_str = now.strftime("%H:%M:%S.%f")
-    sock.sendto(str(time_str).encode(), addr)  # Envia a hora como string
+    sock.sendto(str(time_str).encode(), addr)  # 
